445c1402fee890bfe0a7c4aa98c4cd292ea9f79d
vicision <duncan.j.mclachlan@gmail.com>
2016-11-02 14:15:36 -0400

    Built user input into #menu, hard-coded as test for now

--- lib/nyc_today/cli.rb
+++ lib/nyc_today/cli.rb
@@ -9,14 +9,23 @@ class NycToday::CLI
   end
 
   def list_events
-    puts <<~DOC.gsub /^\s*/, ''
-    1. First event tonight - First venue name - Event time - Price
-    2. Second event tonight - Second venue name - Event time - Price
+    puts <<-DOC.gsub /^\s*/, ''
+      1. First event tonight - First venue name - Event time - Price
+      2. Second event tonight - Second venue name - Event time - Price
     DOC
   end
 
   def menu
     puts "Enter the number of the event for which you'd like more info:"
+    input = gets.strip.downcase
+    if input == "1"
+      puts "1. First event tonight - First venue name - Event time - Price"
+    elsif input == "2"
+      puts "2. Second event tonight - Second venue name - Event time - Price"
+    else
+      exit
+    end
+    call
   end
 
 end
