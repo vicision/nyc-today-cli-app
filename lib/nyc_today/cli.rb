05b96695aee6b4758f692f00f96aed7b9f842c07
vicision <duncan.j.mclachlan@gmail.com>
2016-11-02 14:07:55 -0400

    added #list_events and #menu methods to CLI

--- lib/nyc_today/cli.rb
+++ lib/nyc_today/cli.rb
@@ -4,7 +4,19 @@ class NycToday::CLI
 
   def call
     puts "Today's indie concerts and more in NYC:"
-    puts
+    list_events
+    menu
+  end
+
+  def list_events
+    puts <<~DOC.gsub /^\s*/, ''
+    1. First event tonight - First venue name - Event time - Price
+    2. Second event tonight - Second venue name - Event time - Price
+    DOC
+  end
+
+  def menu
+    puts "Enter the number of the event for which you'd like more info:"
   end
 
 end
