6c41677eba71db21e29fda7620b58502914583e2
vicision <duncan.j.mclachlan@gmail.com>
2016-11-02 14:28:28 -0400

    Refactored #menu and added #goodbye

--- lib/nyc_today/cli.rb
+++ lib/nyc_today/cli.rb
@@ -6,6 +6,7 @@ class NycToday::CLI
     puts "Today's indie concerts and more in NYC:"
     list_events
     menu
+    goodbye
   end
 
   def list_events
@@ -16,16 +17,24 @@ class NycToday::CLI
   end
 
   def menu
-    puts "Enter the number of the event for which you'd like more info:"
-    input = gets.strip.downcase
-    if input == "1"
-      puts "1. First event tonight - First venue name - Event time - Price"
-    elsif input == "2"
-      puts "2. Second event tonight - Second venue name - Event time - Price"
-    else
-      exit
+    input = nil
+    until input == "exit"
+      puts "Enter the number of the event for which you'd like more info or type 'list' to see the events again:"
+      input = gets.strip.downcase
+      if input == "1"
+        puts "1. First event tonight - First venue name - Event time - Price"
+      elsif input == "2"
+        puts "2. Second event tonight - Second venue name - Event time - Price"
+      elsif input == "list"
+        list_events
+      else
+        puts "Input not valid: please enter a number from the list above"
+      end
     end
-    call
+  end
+
+  def goodbye
+    puts "Good-bye! Come back tomorrow for a new list of events"
   end
 
 end
