d47a61268992ed3d9569d609064d1d63b1d3913a
vicision <duncan.j.mclachlan@gmail.com>
2017-01-18 18:22:57 -0500

    More polishing and refactoring

--- lib/nyc_today/cli.rb
+++ lib/nyc_today/cli.rb
@@ -12,19 +12,16 @@ class NycToday::CLI
   def welcome
     system "clear"
     puts "Welcome to NYC Today, your guide to live music"
-    puts "and other events in and around New York City today!"
-    puts
+    puts "and other events in and around New York City today!\n\n"
     puts "Please wait a few seconds while I gather today's events."
     puts "For best results, maximize the terminal."
     system "clear"
   end
 
   def list_event_types
-    puts "Here are today's event categories:"
-    puts
+    puts "Here are today's event categories:\n\n"
     type_entry
-    puts
-    puts "-------------------------------------------------------------"
+    puts "\n-------------------------------------------------------------"
     puts "* Enter a number for the type of event you would like to see."
     puts "* Type 'exit' to leave the program."
     choose_type
@@ -57,8 +54,7 @@ class NycToday::CLI
     page_count
     category
     if @@set_no+1 <= page_count
-      puts "Here's page #{@@set_no+1}/#{page_count} of today's #{category} events ordered by time:"
-      puts
+      puts "Here's page #{@@set_no+1}/#{page_count} of today's #{category} events ordered by time:\n\n"
       event_entry
     else
       end_of_list
@@ -77,8 +73,7 @@ class NycToday::CLI
   def event_entry
     NycToday::Event.event_sets(@@type_choice)[@@set_no].each.with_index(1) do |event, i|
       puts "#{i.to_s.rjust(3," ")} | #{event.name}"
-      puts "    | #{event.time} at #{event.venue}"
-      puts
+      puts "    | #{event.time} at #{event.venue}\n\n"
     end
   end
 
@@ -115,7 +110,7 @@ class NycToday::CLI
   end
 
   def more_events
-    puts "--------------------------------------------------------------"
+    puts "-------------------------------------------------------------------"
     puts "* Enter the number of any event you'd like to know more about."
     puts "* Press Enter for more events"
     puts "* Type 'menu' to return to the main menu, or type 'back' or 'exit'."
@@ -124,19 +119,18 @@ class NycToday::CLI
   def more_info(event)
     if event.event_info != nil || event.price != nil
       system "clear"
-      puts "--------------------------------------------------------------"
-      puts "Ticket info: #{event.price}\n" + "\n" unless event.price == nil
+      puts "\n--------------------------------------------------------------------------------\n\n"
+      puts "Ticket info: #{event.price}\n\n" unless event.price == nil
       puts wrap(event.event_info, 80) unless event.event_info == nil
     else
       system "clear"
-      puts "I'm sorry, there is no additional information about this event."
+      puts "\nI'm sorry, there is no additional information about this event."
     end
     return_to_menu
   end
 
   def return_to_menu
-    puts
-    puts "--------------------------------------------"
+    puts "\n--------------------------------------------------------------------------------"
     puts "* Press Enter to return to the list of events."
     puts "* Type 'exit' to leave the program."
     input = gets.strip.downcase
@@ -149,15 +143,14 @@ class NycToday::CLI
   end
 
   def end_of_list
-    puts "You've reached the end of the #{category} events list. Would you like to see it again? (Y/n)"
+    puts "You've reached the end of the #{category} events list. Would you like to see it again? (Y/n)\n\n"
     input = gets.strip.downcase
     if input == "y"
       @@set_no = 0
       list_events
     elsif input == "n"
-      puts
-      puts "Returning to main menu..."
-      sleep 1
+      puts "\nReturning to main menu..."
+      sleep 1.25
       reset_menu
     else
       error
